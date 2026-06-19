ec39182bc187224a3b42ca1230155659a6ca27d5
unknown <bengkelgadget.l2@gmail.com>
2026-06-19 22:31:47 +0800

    chore: change package name to com.serverpulsa.pro and bump version to 2

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.konter.app',
-  appName: 'Konter Pulsa',
+  appId: 'com.serverpulsa.pro',
+  appName: 'Server Pulsa Pro',
   webDir: 'dist',
   bundledWebRuntime: false,
   plugins: {
