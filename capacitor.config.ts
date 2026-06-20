70c18f744aa2b13f52e623def07bd8b77272314b
unknown <bengkelgadget.l2@gmail.com>
2026-06-20 11:00:52 +0800

    test: change application ID to com.rog.test.app99 to bypass zombie package state on ASUS

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.serverpulsa.pro',
+  appId: 'com.rog.test.app99',
   appName: 'Server Pulsa Pro',
   webDir: 'dist',
   bundledWebRuntime: false,
